6302f54124766a773f9fe9506ea6d8977321b60b
Anson Kao <anson.kao@gmail.com>
2014-10-02 14:36:58 -0400

    Cleaned up Transport singleton + prettied up the Transport UI

--- js/transport.js
+++ js/transport.js
@@ -1,52 +1,55 @@
 var Transport = (function(){
 
-  var notes = [];
   var keyFrequency = [];
   var oscillator = [];
-  var bpm = 130;
-  var playlengthModifier = 1/(bpm/60);
+  var bpm = 128;
   var ctx = new AudioContext();
 
   // Set the frequencies for the notes
-  var setFrequencies = (function(){
-    for( i = 1; i <= 88; i++ )
-    {
-      keyFrequency[i] = Math.pow(2, (i-49)/12) * 440;
-    }
-  })();
-
-  var getPlayTime = function( time ){
-    return playlengthModifier * time + ctx.currentTime;
+  for( i = 1; i <= 88; i++ )
+  {
+    keyFrequency[i] = Math.pow(2, (i-49)/12) * 440;
   }
 
-  var createOscillator = function( key, start, end ){
-    var oscillator = ctx.createOscillator();
-    oscillator.connect(ctx.destination);
-    oscillator.type = 'square';
-    oscillator.frequency.value = keyFrequency[ key ];
-    oscillator.start( getPlayTime( start ) );
-    oscillator.stop( getPlayTime( end ) );
-  }
-
-  var getNotes = function(){
-    notes = PIANO.getNotes( target );
-  };
-
-  var play = function(){
-    getNotes();
-
-    for( i in notes )
+  var getPlayTime = function( time )
     {
-      createOscillator( notes[i].key, notes[i].start, notes[i].end );
-    }
-  };
+      return time * 60 / bpm + ctx.currentTime;
+    };
 
-  var stop = function(){
+  var createOscillator = function( key, start, end )
+    {
+      var oscillator = ctx.createOscillator();
+      oscillator.connect(ctx.destination);
+      oscillator.type = 'square';
+      oscillator.frequency.value = keyFrequency[ key ];
+      oscillator.start( getPlayTime( start ) );
+      oscillator.stop( getPlayTime( end ) );
+    };
 
-  };
+  var play = function()
+    {
+      var notes = PIANO.getNotes( target );
+
+      for( i in notes )
+      {
+        createOscillator( notes[i].key, notes[i].start, notes[i].end );
+      }
+    };
+
+  var stop = function()
+    {
+      // TODO
+    };
+
+  var setTempo = function(tempo)
+    {
+      bpm = tempo;
+    };
 
   // Return the public methods
   return { play: play
+         , stop: stop
+         , setTempo: setTempo
          };
 
 })();

172375a3daa5c39f8f50e47bf7e1a63777979fc2
Anson Kao <anson.kao@gmail.com>
2015-07-18 02:33:50 -0400

    Proper live scheduler in place!

--- js/transport.js
+++ js/transport.js
@@ -8,6 +8,9 @@ var Transport = (function(){
   var ctx = new AudioContext();
   var masterVolume = ctx.createGain();
   var analyser1 = ctx.createAnalyser();
+  var playing = false;
+  var scheduleLoopers = [];
+
 
   // Initialize everything;
   analyser1.connect( masterVolume );
@@ -77,19 +80,41 @@ var Transport = (function(){
 
   var playAll = function()
     {
-      var playStart = ctx.currentTime;
-      var notes = PIANO.getAllNotes();
+      playing = true;
 
-      for( var loop = 0; loop < 100; loop++ )
-      {
-        for( var i in notes )
+      var playStart = ctx.currentTime;
+      var barsPerLoop = 4;
+      var notes = jQuery.extend( true, [], PIANO.getAllNotes() );
+      var loop = 0;
+
+      var newScheduleLooper = setInterval(function(){
+
+        // Schedule up to the next 100 ms worth of notes
+        var startTime = getPlayTime( notes[0].start + loop*barsPerLoop, playStart );
+        var   endTime = getPlayTime( notes[0].end   + loop*barsPerLoop, playStart );
+        while( startTime < ctx.currentTime + 0.100 )
         {
-          var startTime = getPlayTime( notes[i].start + loop*4, playStart );
-          var   endTime = getPlayTime( notes[i].end   + loop*4, playStart );
-          playSingleNote( notes[i].key, notes[i].velocity, startTime, endTime );
+          playSingleNote( notes[0].key, notes[0].velocity, startTime, endTime );
+          notes.splice(0,1);  // Remove from queue
+          if( notes.length < 1)
+            break;
+          startTime = getPlayTime( notes[0].start + loop*barsPerLoop, playStart );
+            endTime = getPlayTime( notes[0].end   + loop*barsPerLoop, playStart );
         }
-      }
+
+        // Reached the end of loop - queue up next loop
+        if( notes.length === 0)
+        {
+          loop++;
+          notes = jQuery.extend( true, [], PIANO.getAllNotes() );
+        }
+
+      }, 50);
+
+      scheduleLoopers.push(newScheduleLooper);
     };
+
+
   var playSingleNote = function( key, velocity, startTime, endTime )
     {
       startTime = startTime || ctx.currentTime;
@@ -143,6 +168,11 @@ var Transport = (function(){
     };
   var stop = function()
     {
+      playing = false;
+      scheduleLoopers.forEach(function(scheduleLooper){
+        clearInterval(scheduleLooper);
+      });
+
       amplitudeEnvelopes.forEach(function(amplitudeEnvelope){
         if( amplitudeEnvelope )
         {
